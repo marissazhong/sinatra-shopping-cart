42351e802cc022669c290142d8e74de9b0f04a3f
Marissa Zhong <marissazhong@Marissas-MBP.fios-router.home>
2018-07-22 16:07:19 -0400

    initialize git

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,21 @@
+require './config/environment'
+require "./app/models/user"
+
+class ApplicationController < Sinatra::Base
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "password_security"
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  helpers do
+
+  end
+
+end
