6d8321a9a52883134f74b7048c0d82c503b8aada
Marissa Zhong <marissazhong@Marissas-MBP.fios-router.home>
2018-07-22 16:27:11 -0400

    add add_column migration to items and create seeds file

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 3) do
+ActiveRecord::Schema.define(version: 4) do
 
   create_table "items", force: :cascade do |t|
     t.string   "name"
     t.integer  "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "size"
   end
 
   create_table "user_items", force: :cascade do |t|
