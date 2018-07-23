234214c7e8c5204005e1a852a73b9493c7bc04df
Marissa Zhong <marissazhong@Marissas-MBP.fios-router.home>
2018-07-23 09:22:13 -0400

    start category controller action and items show view (not working)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,11 +3,16 @@ class ItemsController < ApplicationController
     get '/items' do
         if logged_in?
             @user = current_user
-            @items = @user.items
+            @items = Item.all
             erb :'/items/index'
         else
             redirect to "/login"
         end
     end
 
+    get '/items/:category' do
+        @category = params[:category]
+        @items = Item.all.select {|i| i[:category] == @category}
+        erb :'/items/show'
+    end
 end
