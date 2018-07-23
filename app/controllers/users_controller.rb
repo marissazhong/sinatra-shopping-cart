994e8bb61de8d88f7077b8ae9ede7b9478f8e3c5
Marissa Zhong <marissazhong@Marissas-MBP.fios-router.home>
2018-07-22 21:16:23 -0400

    add user controller actions (not working)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,56 @@
 class UsersController < ApplicationController
 
+    get '/users/:slug' do
+        @user = User.find(params[:slug])
+        erb :'/users/show'
+    end
+    
+    get '/signup' do
+        if logged_in?
+            redirect to "/items"
+        else
+            erb :'/users/create_user'
+        end
+    end
+    
+    post '/signup' do
+        if !params[:username].empty? && !params[:email].empty? && !params[:password].empty?
+            @user = User.create(username: params[:username], email: params[:email], password: params[:password])
+            @user.save
+            session[:user_id] = @user.id
+            redirect to "/items"
+        else
+            redirect to "/signup"
+        end
+    end
+    
+    get '/login' do
+        if logged_in?
+            redirect to "/items"
+        else
+            erb :'/users/login'
+        end
+    end
+    
+    post '/login' do
+        @user = User.find_by(username: params[:username])
+        if @user && @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+            redirect to "/items"
+        else
+            redirect to "/signup"
+        end
+    end
+    
+    get '/logout' do
+        if logged_in?
+            session.clear
+            redirect to "/login"
+        else
+            redirect to "/"
+        end
+    end
+    
+       
+
 end
