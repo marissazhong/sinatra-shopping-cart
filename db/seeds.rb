a3a4f696dd8ca2a6b884add286aebfb63a94214f
Marissa Zhong <marissazhong@Marissas-MBP.fios-router.home>
2018-07-22 20:15:48 -0400

    start adding items to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,15 @@
 
 items_database = [
-    {type: 'Fats and Oils', items: [{name: '', size: '', price: ''}, ]},
+    {type: 'Fats and Oils', items: 
+        [{name: 'Olive Oil', size: '32oz', price: '8.99'}, 
+        {name: 'Vegetable Oil', size: '32oz', price: '2.99'},
+        {name: 'Butter', size: '1lb', price: '2.99'}]},
 
-    {type: 'Meat', items: [{name: 'Eggs', size: '12ct', price: '1.69'}, ]},
+    {type: 'Meat', items: 
+        [{name: 'Eggs', size: '12ct', price: '1.69'}, 
+        {name: 'Beef', size: '2lbs', price: '7.99'},
+        {name: 'Chicken', size: '2lbs', price: '8.99'},
+        {name: 'Pork', size: '1lb', price: '5.99'}]},
 
     {type: 'Fish', items: [{name: '', size: '', price: ''}, ]},
 
@@ -18,3 +25,9 @@ items_database = [
 
     {type: 'Fruits', items: [{name: '', size: '', price: ''}, ]}
 ]
+
+items_database.each do |items_type|
+    items_type.items.each do |item|
+        Item.create(name: item.name, size: item.size, price: item.price, type: items_type.type)
+    end
+end
