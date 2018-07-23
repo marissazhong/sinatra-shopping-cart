86b609caa32b49871c462ce19b28dcd5f96d4227
Marissa Zhong <marissazhong@Marissas-MBP.fios-router.home>
2018-07-22 21:33:45 -0400

    add item controller action for index

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,3 +1,13 @@
 class ItemsController < ApplicationController
 
+    get '/items' do
+        if logged_in?
+            @user = current_user
+            @items = @user.items
+            erb :'/items/index'
+        else
+            redirect to "/login"
+        end
+    end
+
 end
